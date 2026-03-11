50b28f34a76373cb7a716c22ae8ce6c06d7565aa
WASDetchan <yarik.antipov2008@gmail.com>
2026-03-11 21:54:11 +0300

    a little homepage

--- main.go
+++ main.go
@@ -1,24 +1,33 @@
 package main
 
 import (
-	"io"
+	"context"
 	"log"
 	"net/http"
+
+	"github.com/WASDetchan/wasdetchan-online/pages"
+	"github.com/a-h/templ"
 )
 
 func main() {
-	// Hello world, the web server
+	http.Handle("/home", templ.Handler(pages.Home()))
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
-	}
+	home := pages.Home()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
 
-	http.HandleFunc("/hello", helloHandler)
+		home.Render(context.Background(), w)
+	})
 
 	http.HandleFunc("/feed.yml", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/xml")
 		http.ServeFile(w, r, "/public/feed.yml")
 	})
+
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("/public"))))
 
 	log.Fatal(http.ListenAndServe(":8082", nil))
 }
